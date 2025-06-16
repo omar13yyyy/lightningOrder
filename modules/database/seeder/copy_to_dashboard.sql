75061a43d51296a4ad2a62a3047f4ed10c838e7b
omar alhaj ali <omar18yyyy@gmail.com>
2025-06-16 19:58:36 +0300

    after merge

--- modules/database/seeder/copy_to_dashboard.sql
+++ modules/database/seeder/copy_to_dashboard.sql
@@ -24,6 +24,8 @@ COPY document_images FROM '/home/omar/project3/coding/backend/modules/database/s
 COPY coupons FROM '/home/omar/project3/coding/backend/modules/database/seeder/CSV/coupons.csv' DELIMITER ',' CSV HEADER;
 COPY withdrawal_requests FROM '/home/omar/project3/coding/backend/modules/database/seeder/CSV/withdrawal_requests.csv' DELIMITER ',' CSV HEADER;
 COPY withdrawal_document_images FROM '/home/omar/project3/coding/backend/modules/database/seeder/CSV/withdrawal_document_images.csv' DELIMITER ',' CSV HEADER;
+COPY system_settings FROM '/home/omar/project3/coding/backend/modules/database/seeder/CSV/system_settings.csv' DELIMITER ',' CSV HEADER;
+
 //---------------------------------------
 COPY products_sold FROM 'E:\lightningOrder/modules/database/seeder/CSV/products_sold.csv' DELIMITER ',' CSV HEADER;
 COPY daily_statistics FROM 'E:\lightningOrder/modules/database/seeder/CSV/daily_statistics.csv' DELIMITER ',' CSV HEADER;
@@ -45,8 +47,8 @@ COPY partners FROM 'E:\lightningOrder/modules/database/seeder/CSV/partners.csv' 
 COPY statistics_previous_day FROM 'E:\lightningOrder/modules/database/seeder/CSV/statistics_previous_day.csv' DELIMITER ',' CSV HEADER;
 COPY withdrawal_requests FROM 'E:\lightningOrder/modules/database/seeder/CSV/withdrawal_requests.csv' DELIMITER ',' CSV HEADER;
 COPY withdrawal_document_images FROM 'E:\lightningOrder/modules/database/seeder/CSV/withdrawal_document_images.csv' DELIMITER ',' CSV HEADER;
+COPY system_settings FROM 'E:\lightningOrder/modules/database/seeder/CSV/system_settings.csv' DELIMITER ',' CSV HEADER;
 
-COPY system_settings FROM '/home/omar/project3/coding/backend/modules/database/seeder/CSV/system_settings.csv' DELIMITER ',' CSV HEADER;
 
 
 
